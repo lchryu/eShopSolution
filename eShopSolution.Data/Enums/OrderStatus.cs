5a73e6fecaa0d49b99b91bcda39e9f88bd2b1521
Lucute2001 <lch.ryu2001@gmail.com>
2023-04-16 14:58:19 +0700

    Add Entity

---
+++ eShopSolution.Data/Enums/OrderStatus.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Enums
+{
+    public enum OrderStatus
+    {
+        InProgress,
+        Confirmed,
+        Shipping,
+        Success,
+        Canceled
+    }
+}
