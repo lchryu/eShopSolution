f38a40255dca9f3f91de462a97b5374a534a2ef1
Lucute2001 <lch.ryu2001@gmail.com>
2023-04-21 12:27:52 +0700

    Complete application for product

--- eShopSolution.Application/Catalog/Products/IManageProductService.cs
+++ eShopSolution.Application/Catalog/Products/IManageProductService.cs
@@ -1,9 +1,6 @@
 ﻿using eShopSolution.Application.Catalog.Products.Dtos;
 using eShopSolution.Application.Catalog.Products.Dtos.Manage;
 using eShopSolution.Application.Dtos;
-using System;
-using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
 namespace eShopSolution.Application.Catalog.Products
 {
@@ -22,7 +19,6 @@ namespace eShopSolution.Application.Catalog.Products
 
         Task AddViewcount(int productId);
 
-        Task<List<ProductViewModel>> GetAll();
 
         Task<PagedResult<ProductViewModel>> GetAllPaging(GetProductPagingRequest request);
     }
